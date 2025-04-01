55f8f4949a3f45f6c316066994bdf7399e248ac9
adami <adamidrissi177@gmail.com>
2025-04-01 16:06:30 +0000

    Add description on the new columns

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+
+st.title('Adam Idrissi')
+content = """
+Hello I am Adam Idrissi. I just started learning how to code in Python and I am currently 13 years old in 8th grade.
+"""
+st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python that will continue to update. 
+Feel free to contact me!
+"""
+
+st.write(content2)
