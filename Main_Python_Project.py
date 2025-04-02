9fe07b3d6dcee79c98e6d7ebc0e16cb1ca5d763c
adami <adamidrissi177@gmail.com>
2025-04-02 21:17:47 +0000

    Add big improvements

--- Main_Python_Project.py
+++ Main_Python_Project.py
@@ -6,7 +6,9 @@ st.set_page_config(layout="wide")
 
 st.title('Adam Idrissi')
 content = """
-Hello I am Adam Idrissi. I just started learning how to code in Python and I am currently 13 years old in 8th grade.
+Hello I am Adam Idrissi. I just started learning how to code in Python and I am currently 13 years old.
+I will be learning other languages than python so the side bar will keep on updating and 
+there will be new projects each week.
 """
 st.info(content)
 
@@ -19,7 +21,7 @@ st.write(content2)
 
 col1, empty_column, col2 = st.columns([1.5,0.5,1.5])
 
-df = pandas.read_csv("data.csv", sep=";")
+df = pandas.read_csv(r"C:\Users\adami\PycharmProjects\PythonProject2\data.csv", sep=";")
 
 with col1:
     for index, row in df[:10].iterrows():
