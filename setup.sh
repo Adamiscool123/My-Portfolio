e8827bb92bdb558fcf6431d0b0bd3cee02cc3a19
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-03 11:46:33 +0000

    Add configuration files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 mkdir -p ~/.streamlit/
 
+# shellcheck disable=SC2028
 echo "\
 [general]\n\
 email = \"adamidrissi177@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
+# shellcheck disable=SC2028
 echo "\
 [server]\n\
 headless = true\n\
