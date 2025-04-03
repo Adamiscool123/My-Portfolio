b428ab5a13c42458e7679215d1e2b938316058b0
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-03 11:45:31 +0000

    Add configuration files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"adamidrissi177@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
