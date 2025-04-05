e394123433ec94c61fe900d6c6a2eef1e123477c
Adamiscool123 <adamidrissi177@gmail.com>
2025-04-05 13:35:09 +0000

    Made final adjustments

--- send_email.py
+++ send_email.py
@@ -3,10 +3,10 @@ import smtplib, ssl
 from dotenv import load_dotenv
 
 load_dotenv()
+password = os.getenv("PASSWORD")
 def send_email(message, email):
     host = "smtp.gmail.com"
     port = 465
-    password = os.getenv("PASSWORD")
     receiver = 'adamidrissi177@gmail.com'
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
